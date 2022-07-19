14c4465f913498e2671dbe8874a90868cae3d676
sula92 <sulakkanaid@gmail.com>
2022-07-20 02:16:34 +0530

    mng prod

--- EShift/ManageProduct.Designer.cs
+++ EShift/ManageProduct.Designer.cs
@@ -28,127 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.txtQuantity = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtProductName = new System.Windows.Forms.TextBox();
-            this.txtJobId = new System.Windows.Forms.TextBox();
             this.btnAddNew = new System.Windows.Forms.Button();
             this.btnDelete = new System.Windows.Forms.Button();
             this.btnUpdate = new System.Windows.Forms.Button();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.txtProductName = new System.Windows.Forms.TextBox();
+            this.txtJobID = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnBack = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.Maroon;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button1.Location = new System.Drawing.Point(684, 22);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 33);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Back";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.txtQuantity);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.txtProductName);
-            this.panel1.Controls.Add(this.txtJobId);
             this.panel1.Controls.Add(this.btnAddNew);
             this.panel1.Controls.Add(this.btnDelete);
             this.panel1.Controls.Add(this.btnUpdate);
-            this.panel1.Location = new System.Drawing.Point(1, 1);
+            this.panel1.Controls.Add(this.txtQuantity);
+            this.panel1.Controls.Add(this.txtProductName);
+            this.panel1.Controls.Add(this.txtJobID);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(296, 448);
-            this.panel1.TabIndex = 1;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label3.Location = new System.Drawing.Point(18, 326);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(54, 13);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Quantity";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label2.Location = new System.Drawing.Point(9, 208);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(87, 13);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Product Name";
-            // 
-            // txtQuantity
-            // 
-            this.txtQuantity.Location = new System.Drawing.Point(102, 319);
-            this.txtQuantity.Name = "txtQuantity";
-            this.txtQuantity.Size = new System.Drawing.Size(168, 20);
-            this.txtQuantity.TabIndex = 4;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label1.Location = new System.Drawing.Point(11, 91);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 13);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Job ID";
-            // 
-            // txtProductName
-            // 
-            this.txtProductName.Location = new System.Drawing.Point(102, 205);
-            this.txtProductName.Name = "txtProductName";
-            this.txtProductName.Size = new System.Drawing.Size(168, 20);
-            this.txtProductName.TabIndex = 4;
-            // 
-            // txtJobId
-            // 
-            this.txtJobId.Location = new System.Drawing.Point(96, 84);
-            this.txtJobId.Name = "txtJobId";
-            this.txtJobId.Size = new System.Drawing.Size(168, 20);
-            this.txtJobId.TabIndex = 4;
+            this.panel1.Size = new System.Drawing.Size(272, 451);
+            this.panel1.TabIndex = 0;
             // 
             // btnAddNew
             // 
-            this.btnAddNew.BackColor = System.Drawing.Color.DarkBlue;
+            this.btnAddNew.BackColor = System.Drawing.Color.RoyalBlue;
             this.btnAddNew.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAddNew.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btnAddNew.Location = new System.Drawing.Point(11, 21);
+            this.btnAddNew.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnAddNew.Location = new System.Drawing.Point(24, 22);
             this.btnAddNew.Name = "btnAddNew";
-            this.btnAddNew.Size = new System.Drawing.Size(75, 33);
-            this.btnAddNew.TabIndex = 3;
-            this.btnAddNew.Text = "+AddNew";
+            this.btnAddNew.Size = new System.Drawing.Size(75, 32);
+            this.btnAddNew.TabIndex = 1;
+            this.btnAddNew.Text = "+Add New";
             this.btnAddNew.UseVisualStyleBackColor = false;
-            this.btnAddNew.Click += new System.EventHandler(this.button5_Click);
+            this.btnAddNew.Click += new System.EventHandler(this.btnAddNew_Click);
             // 
             // btnDelete
             // 
-            this.btnDelete.BackColor = System.Drawing.Color.Red;
+            this.btnDelete.BackColor = System.Drawing.Color.Crimson;
             this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnDelete.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btnDelete.Location = new System.Drawing.Point(189, 392);
+            this.btnDelete.Location = new System.Drawing.Point(184, 366);
             this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(75, 31);
+            this.btnDelete.Size = new System.Drawing.Size(75, 33);
             this.btnDelete.TabIndex = 1;
             this.btnDelete.Text = "Delete";
             this.btnDelete.UseVisualStyleBackColor = false;
@@ -156,37 +89,101 @@
             // 
             // btnUpdate
             // 
-            this.btnUpdate.BackColor = System.Drawing.Color.BlueViolet;
+            this.btnUpdate.BackColor = System.Drawing.Color.Navy;
             this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnUpdate.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btnUpdate.Location = new System.Drawing.Point(21, 392);
+            this.btnUpdate.Location = new System.Drawing.Point(24, 366);
             this.btnUpdate.Name = "btnUpdate";
-            this.btnUpdate.Size = new System.Drawing.Size(75, 31);
-            this.btnUpdate.TabIndex = 0;
+            this.btnUpdate.Size = new System.Drawing.Size(75, 33);
+            this.btnUpdate.TabIndex = 1;
             this.btnUpdate.Text = "Update";
             this.btnUpdate.UseVisualStyleBackColor = false;
-            this.btnUpdate.Click += new System.EventHandler(this.button2_Click);
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Location = new System.Drawing.Point(93, 285);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(166, 20);
+            this.txtQuantity.TabIndex = 2;
+            // 
+            // txtProductName
+            // 
+            this.txtProductName.Location = new System.Drawing.Point(93, 191);
+            this.txtProductName.Name = "txtProductName";
+            this.txtProductName.Size = new System.Drawing.Size(166, 20);
+            this.txtProductName.TabIndex = 2;
+            // 
+            // txtJobID
+            // 
+            this.txtJobID.Location = new System.Drawing.Point(93, 92);
+            this.txtJobID.Name = "txtJobID";
+            this.txtJobID.Size = new System.Drawing.Size(166, 20);
+            this.txtJobID.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(3, 292);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Quantity";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(3, 198);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Product Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(3, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Job ID";
             // 
             // dataGridView1
             // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(316, 151);
+            this.dataGridView1.Location = new System.Drawing.Point(311, 132);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(457, 150);
-            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.Size = new System.Drawing.Size(464, 150);
+            this.dataGridView1.TabIndex = 1;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btnBack
+            // 
+            this.btnBack.BackColor = System.Drawing.Color.Red;
+            this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnBack.Location = new System.Drawing.Point(700, 22);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 32);
+            this.btnBack.TabIndex = 2;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = false;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
             // 
             // ManageProduct
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBack);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.button1);
             this.Name = "ManageProduct";
             this.Text = "ManageProduct";
             this.Load += new System.EventHandler(this.ManageProduct_Load);
@@ -199,17 +196,17 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox txtQuantity;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txtProductName;
-        private System.Windows.Forms.TextBox txtJobId;
-        private System.Windows.Forms.Button btnAddNew;
         private System.Windows.Forms.Button btnDelete;
         private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.TextBox txtProductName;
+        private System.Windows.Forms.TextBox txtJobID;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAddNew;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnBack;
     }
 }
--- EShift/ManageProduct.cs
+++ EShift/ManageProduct.cs
@@ -10,10 +10,9 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace EShift
-{
+{    
     public partial class ManageProduct : Form
     {
-
         private DataTable dt;
         private MySqlConnection connection = DBConnection.getInstance().getConnection();
         private String jobId = ManageJob.jobId;
@@ -23,22 +22,147 @@ namespace EShift
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void ManageProduct_Load(object sender, EventArgs e)
         {
-            this.Hide();
-            AdminDashboard adminDashboard = new AdminDashboard();
-            adminDashboard.Show();
+            loadTableDta();
+            dataGridView1.Columns[0].DefaultCellStyle.ForeColor = Color.Blue;
+            dataGridView1.Columns[1].DefaultCellStyle.ForeColor = Color.Blue;
+
+
+            dataGridView1.ColumnHeadersDefaultCellStyle.BackColor = Color.Black;
+            dataGridView1.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+            dataGridView1.EnableHeadersVisualStyles = false;
+
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridView1.BackgroundColor = SystemColors.ButtonShadow;
+            dataGridView1.BorderStyle = BorderStyle.None;
+            dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+
+            btnDelete.Enabled = false;
+            btnUpdate.Enabled = false;
+            txtJobID.Text = jobId;
+            txtJobID.ReadOnly = true;
         }
 
-        private void button5_Click(object sender, EventArgs e)
+        private void btnAddNew_Click(object sender, EventArgs e)
         {
-            
 
             btnUpdate.Enabled = true;
             btnDelete.Enabled = false;
-            clearAll();          
+            clearAll();
             btnUpdate.Text = "Save";
             loadTableDta();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (connection != null)
+            {
+                connection.Close();
+            }
+
+            if (btnUpdate.Text == "Update")
+            {
+
+                connection.Open();
+                string updateProduct = ("UPDATE `eshift`.`product` SET `quantity`='" + txtQuantity.Text + "' WHERE `job_id`  = '" + txtJobID.Text + "' AND `product_name`  = '" + txtProductName.Text + "' ");
+                MySqlCommand update_cmd = new MySqlCommand(updateProduct, connection);
+                int res_update = update_cmd.ExecuteNonQuery();
+                connection.Close();
+                if (res_update == 1)
+                {
+                    loadTableDta();
+                    dataGridView1.Refresh();
+                    MessageBox.Show("Job updated sucessfully...");
+                }
+                else
+                {
+                    MessageBox.Show("Failed to update record....");
+                }
+
+            }
+            else
+            {
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    if (txtProductName.Text == row.Cells["product_name"].Value.ToString())
+                    {
+                        MessageBox.Show("You cant duplicate the same product");
+                        return;
+                    }
+
+                }
+
+                connection.Open();
+                MySqlCommand com_customer = new MySqlCommand("insert into product (job_id,product_name,quantity) values ('" + txtJobID.Text + "','" + txtProductName.Text + "','" + txtQuantity.Text + "')", connection);
+                int resSave = com_customer.ExecuteNonQuery();
+                connection.Close();
+                if (resSave == 1)
+                {
+                    clearAll();
+                    loadTableDta();
+                    dataGridView1.Refresh();
+                    MessageBox.Show("Record Inserted Successfully...!");
+                }
+                else
+                {
+                    MessageBox.Show("Failed To Inser The Record...!");
+                }
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("HI");
+            try
+            {
+                if (connection != null)
+                {
+                    connection.Close();
+                }
+                connection.Open();
+
+                string deleteCmd = ("DELETE FROM `product` WHERE `product`.`job_id` = '"+txtJobID.Text+"' AND `product`.`product_name` = '"+txtProductName.Text+"'");
+                MySqlCommand delete_cmd = new MySqlCommand(deleteCmd, connection);
+
+                int res_update = delete_cmd.ExecuteNonQuery();
+                connection.Close();
+                if (res_update == 1)
+                {
+                    clearAll();
+                    loadTableDta();
+                    dataGridView1.Refresh();
+                    MessageBox.Show("Record deleted sucessfully...");
+                }
+                else
+                {
+                    MessageBox.Show("Failed to remove record...");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An Exception has occurred : {0}", ex.Message);
+            }
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            ManageJob manageJob = new ManageJob();
+            manageJob.Show();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            MessageBox.Show(dt.Rows[e.RowIndex].Field<int>(2).ToString());
+            txtJobID.Text = dt.Rows[e.RowIndex].Field<String>(0).ToString();
+            txtProductName.Text = dt.Rows[e.RowIndex].Field<String>(1).ToString();
+            txtQuantity.Text = dt.Rows[e.RowIndex].Field<int>(2).ToString();
+
+            btnUpdate.Text = "Update";
+            btnUpdate.Enabled = true;
         }
 
         private void loadTableDta()
@@ -69,141 +193,8 @@ namespace EShift
         private void clearAll()
         {
             txtProductName.Clear();
-            txtQuantity.Clear();            
+            txtQuantity.Clear();
             btnUpdate.Text = "Update";
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            if (connection != null)
-            {
-                connection.Close();
-            }
-            
-            if (btnUpdate.Text == "Update")
-            {
-
-                connection.Open();
-                string updateProduct = ("UPDATE `eshift`.`product` SET `quantity`='" + txtQuantity.Text + "' WHERE `job_id`  = '" + txtJobId.Text + "' AND `product_name`  = '" + txtProductName.Text + "' ");
-                MySqlCommand update_cmd = new MySqlCommand(updateProduct, connection);
-                int res_update = update_cmd.ExecuteNonQuery();
-                connection.Close();
-                if (res_update == 1)
-                {
-                    loadTableDta();
-                    dataGridView1.Refresh();
-                    MessageBox.Show("Job updated sucessfully...");
-                }
-                else
-                {
-                    MessageBox.Show("Failed to update record....");
-                }
-
-            }
-            else
-            {
-                foreach (DataGridViewRow row in dataGridView1.Rows)
-                {
-                    if (txtProductName.Text == row.Cells["product_name"].Value.ToString())
-                    {
-                        MessageBox.Show("You cant duplicate the same product");
-                        return;
-                    }
-
-                }
-
-                connection.Open();
-                MySqlCommand com_customer = new MySqlCommand("insert into product (job_id,product_name,quantity) values ('" + txtJobId.Text + "','" +txtProductName.Text + "','" + txtQuantity.Text + "')", connection);
-                int resSave = com_customer.ExecuteNonQuery();
-                connection.Close();
-                if (resSave == 1)
-                {
-                    clearAll();
-                    loadTableDta();
-                    dataGridView1.Refresh();
-                    MessageBox.Show("Record Inserted Successfully...!");
-                }
-                else
-                {
-                    MessageBox.Show("Failed To Inser The Record...!");
-                }
-            }
-        }
-
-        private void button6_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("HI");
-            try
-            {
-                if (connection != null)
-                {
-                    connection.Close();
-                }
-                connection.Open();
-                
-                string deleteCmd = ("DELETE FROM `product` WHERE `product`.`job_id` = 'J002' AND `product`.`product_name` = 'www'");
-                MySqlCommand delete_cmd = new MySqlCommand(deleteCmd, connection);
-
-                int res_update = delete_cmd.ExecuteNonQuery();
-                connection.Close();
-                if (res_update == 1)
-                {
-                    clearAll();
-                    loadTableDta();
-                    dataGridView1.Refresh();
-                    MessageBox.Show("Record deleted sucessfully...");
-                }
-                else
-                {
-                    MessageBox.Show("Failed to remove record...");
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("An Exception has occurred : {0}", ex.Message);
-            }
-        }
-
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-            MessageBox.Show(dt.Rows[e.RowIndex].Field<int>(2).ToString());
-                txtJobId.Text = dt.Rows[e.RowIndex].Field<String>(0).ToString();
-                txtProductName.Text = dt.Rows[e.RowIndex].Field<String>(1).ToString();
-                txtQuantity.Text = dt.Rows[e.RowIndex].Field<int>(2).ToString();
-
-                btnUpdate.Text = "Update";
-                btnUpdate.Enabled = true;       
-        }
-
-        private void ManageProduct_Load(object sender, EventArgs e)
-        {
-            loadTableDta();
-            dataGridView1.Columns[0].DefaultCellStyle.ForeColor = Color.Blue;
-            dataGridView1.Columns[1].DefaultCellStyle.ForeColor = Color.Blue;
-
-
-            dataGridView1.ColumnHeadersDefaultCellStyle.BackColor = Color.Black;
-            dataGridView1.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
-            dataGridView1.EnableHeadersVisualStyles = false;
-
-            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
-            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
-            dataGridView1.BackgroundColor = SystemColors.ButtonShadow;
-            dataGridView1.BorderStyle = BorderStyle.None;
-            dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-
-            btnDelete.Enabled = false;
-            btnUpdate.Enabled = false;
-            txtJobId.Text = jobId;
-            txtJobId.ReadOnly = true;
-        }
-
-        private void btnDelete_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("mxmxmxm");
-            
         }
     }
 }
